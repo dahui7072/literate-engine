9d0970034ef81c8926aa412b7f458a4e312f1f75
dahee <kdh100670@gmail.com>
2026-04-05 19:34:07 +0900

    fix error

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 a = input("숫자 입력: ")
 b = input("숫자 입력: ")
 
 print("합:", a + b)
+=======
+a = float(input("숫자 입력: "))
+b = float(input("숫자 입력: "))
+
+print("합:", a + b)
+>>>>>>> 34afd06 (fix error)
